93a582477a12034e0a2cf986dbb1e68e8cd4474a
Thibaud Lemaire <thibaud.lemaire@telecom-paristech.fr>
2020-03-19 22:09:02 +0100

    ES6 syntax

--- back/app.js
+++ back/app.js
@@ -1,22 +1,20 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+import express from "express";
+import createError from "http-errors";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
-const indexRouter = require('./routes/index');
+import { router } from "./routes/index.js";
 
 const app = express();
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter.router);
-
+app.use("/", router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -27,11 +25,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.send({error: 'error'});
+  res.send({ error: "error" });
 });
 
-module.exports = app;
+export default app;
--- back/routes/index.js
+++ back/routes/index.js
@@ -1,63 +1,45 @@
-const express = require('express');
+import express from "express";
+import { Client } from "pg";
+
 const router = express.Router();
 
-const { Client } = require('pg');
-client = new Client({
-    connectionString: process.env.DATABASE_URL
+const client = new Client({
+  connectionString: process.env.DATABASE_URL
 });
 
 client.connect();
 
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-
-    res.send({ hello: "Hello World!"});
-
+router.get("/", function(req, res, next) {
+  res.send({ hello: "Hello World!" });
 });
 
+router.post("/value", function(req, res, next) {
+  let value = req.body;
 
-router.post('/value', function(req, res, next) {
-
-    let value = req.body;
-
-    addValue(value, function (status) {
-        res.sendStatus(status);
-    });
-
+  addValue(value, function(status) {
+    res.sendStatus(status);
+  });
 });
-
 
 async function addValue(value, statusCallback) {
+  if (parseFloat(value.value) === value.value) {
+    let status = 200;
 
-    if (parseFloat(value.value) === value.value){
-
-        let status = 200;
-
-        try {
-
-            await client.query('INSERT INTO values (value, unit, sensor, date) ' +
-                'VALUES ($1, $2, (SELECT  name FROM sensors WHERE token = $3), NOW());',
-                [value.value, value.unit, value.token]);
-
-        } catch(error) {
-
-            status = 400;
-
-        }
-
-        statusCallback(status);
-
-    } else {
-
-        statusCallback(400);
-
+    try {
+      await client.query(
+        "INSERT INTO values (value, unit, sensor, date) " +
+          "VALUES ($1, $2, (SELECT  name FROM sensors WHERE token = $3), NOW());",
+        [value.value, value.unit, value.token]
+      );
+    } catch (error) {
+      status = 400;
     }
 
+    statusCallback(status);
+  } else {
+    statusCallback(400);
+  }
 }
 
-
-module.exports = {
-    router,
-    addValue
-};
+export { router, addValue };
